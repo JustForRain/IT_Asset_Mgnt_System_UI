32b464a4bef46d6338bdfab4c09fcf217c12bea9
yu.xia <rain@pcrain.com>
2025-03-26 17:25:29 +0800

    完善设备账户关联表查询

--- src/views/iams/iamsAccount/index.vue
+++ src/views/iams/iamsAccount/index.vue
@@ -3,54 +3,61 @@
     <div class="layout-padding-auto layout-padding-view">
       <el-row>
         <div class="mb8" style="width: 100%">
-          <el-button icon="folder-add" type="primary" class="ml10" @click="formDialogRef.openDialog()"
-            v-auth="'iams_iamsAccount_add'">
+          <el-button v-auth="'iams_iamsAccount_add'" class="ml10" icon="folder-add" type="primary"
+                     @click="formDialogRef.openDialog()">
             新 增
           </el-button>
-          <el-button plain :disabled="multiple" icon="Delete" type="primary"
-            v-auth="'iams_iamsAccount_del'" @click="handleDelete(selectObjs)">
+          <el-button v-auth="'iams_iamsAccount_del'" :disabled="multiple" icon="Delete" plain
+                     type="primary" @click="handleDelete(selectObjs)">
             删除
           </el-button>
           <right-toolbar v-model:showSearch="showSearch" :export="'iams_iamsAccount_export'"
-                @exportExcel="exportExcel" class="ml10 mr20" style="float: right;"
-            @queryTable="getDataList"></right-toolbar>
+                         class="ml10 mr20" style="float: right;" @exportExcel="exportExcel"
+                         @queryTable="getDataList"></right-toolbar>
         </div>
       </el-row>
-      <el-table :data="state.dataList" v-loading="state.loading" border 
-        :cell-style="tableStyle.cellStyle" :header-cell-style="tableStyle.headerCellStyle"
-				@selection-change="selectionChangHandle"
-        @sort-change="sortChangeHandle">
-        <el-table-column type="selection" width="40" align="center" />
-        <el-table-column type="index" label="#" width="40" />
-          <el-table-column prop="role" label="设备角色"  show-overflow-tooltip/>
-          <el-table-column prop="type" label="类型"  show-overflow-tooltip/>
-          <el-table-column prop="protocol" label="协议"  show-overflow-tooltip/>
-          <el-table-column prop="url" label="地址"  show-overflow-tooltip/>
-          <el-table-column prop="port" label="端口"  show-overflow-tooltip/>
-          <el-table-column prop="account" label="账号"  show-overflow-tooltip/>
-          <el-table-column prop="password" label="密码"  show-overflow-tooltip/>
+      <el-table v-loading="state.loading" :cell-style="tableStyle.cellStyle" :data="state.dataList"
+                :header-cell-style="tableStyle.headerCellStyle" border
+                @selection-change="selectionChangHandle"
+                @sort-change="sortChangeHandle">
+        <el-table-column align="center" type="selection" width="40"/>
+        <el-table-column label="#" type="index" width="40"/>
+        <el-table-column label="设备角色" prop="role" show-overflow-tooltip>
+          <template #default="scope">
+            <!-- 直接绑定样式 -->
+            <div style="white-space: pre-line;">{{ scope.row.role }}</div>
+          </template>
+        </el-table-column>
+        <el-table-column label="类型" prop="type" show-overflow-tooltip/>
+        <el-table-column label="协议" prop="protocol" show-overflow-tooltip/>
+        <el-table-column label="地址" prop="url" show-overflow-tooltip/>
+        <el-table-column label="端口" prop="port" show-overflow-tooltip/>
+        <el-table-column label="账号" prop="account" show-overflow-tooltip/>
+        <el-table-column label="密码" prop="password" show-overflow-tooltip/>
         <el-table-column label="操作" width="150">
           <template #default="scope">
-            <el-button icon="edit-pen" text type="primary" v-auth="'iams_iamsAccount_edit'"
-              @click="formDialogRef.openDialog(scope.row.id)">编辑</el-button>
-            <el-button icon="delete" text type="primary" v-auth="'iams_iamsAccount_del'" @click="handleDelete([scope.row.id])">删除</el-button>
+            <el-button v-auth="'iams_iamsAccount_edit'" icon="edit-pen" text type="primary"
+                       @click="formDialogRef.openDialog(scope.row.id)">编辑
+            </el-button>
+            <el-button v-auth="'iams_iamsAccount_del'" icon="delete" text type="primary"
+                       @click="handleDelete([scope.row.id])">删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
-      <pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" v-bind="state.pagination" />
+      <pagination v-bind="state.pagination" @size-change="sizeChangeHandle" @current-change="currentChangeHandle"/>
     </div>
 
     <!-- 编辑、新增  -->
-    <form-dialog ref="formDialogRef" @refresh="getDataList(false)" />
+    <form-dialog ref="formDialogRef" @refresh="getDataList(false)"/>
 
   </div>
 </template>
 
-<script setup lang="ts" name="systemIamsAccount">
-import { BasicTableProps, useTable } from "/@/hooks/table";
-import { fetchList, delObjs } from "/@/api/iams/iamsAccount";
-import { useMessage, useMessageBox } from "/@/hooks/message";
-import { useDict } from '/@/hooks/dict';
+<script lang="ts" name="systemIamsAccount" setup>
+import {BasicTableProps, useTable} from "/@/hooks/table";
+import {delObjs, fetchList} from "/@/api/iams/iamsAccount";
+import {useMessage, useMessageBox} from "/@/hooks/message";
 
 // 引入组件
 const FormDialog = defineAsyncComponent(() => import('./form.vue'));
@@ -77,7 +84,7 @@ const {
   sizeChangeHandle,
   sortChangeHandle,
   downBlobFile,
-	tableStyle
+  tableStyle
 } = useTable(state)
 
 // 清空搜索条件
@@ -91,12 +98,12 @@ const resetQuery = () => {
 
 // 导出excel
 const exportExcel = () => {
-  downBlobFile('/iams/iamsAccount/export',Object.assign(state.queryForm, { ids: selectObjs }), 'iamsAccount.xlsx')
+  downBlobFile('/iams/iamsAccount/export', Object.assign(state.queryForm, {ids: selectObjs}), 'iamsAccount.xlsx')
 }
 
 // 多选事件
 const selectionChangHandle = (objs: { id: string }[]) => {
-  selectObjs.value = objs.map(({ id }) => id);
+  selectObjs.value = objs.map(({id}) => id);
   multiple.value = !objs.length;
 };
 
