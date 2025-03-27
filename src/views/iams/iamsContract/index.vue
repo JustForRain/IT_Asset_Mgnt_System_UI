883f63da1a06c9c24a636f5ded8ea342ba56794a
yu.xia <rain@pcrain.com>
2025-03-27 16:20:59 +0800

    完成合同和上架模块条件查询接口前后端联调

--- src/views/iams/iamsContract/index.vue
+++ src/views/iams/iamsContract/index.vue
@@ -1,6 +1,28 @@
 <template>
   <div class="layout-padding">
     <div class="layout-padding-auto layout-padding-view">
+       <el-row v-show="showSearch">
+        <el-form :model="state.queryForm" ref="queryRef" :inline="true" @keyup.enter="getDataList">
+      <el-form-item label="甲方" prop="purchaser" >
+        <el-input placeholder="请输入甲方" v-model="state.queryForm.purchaser" />
+      </el-form-item>
+      <el-form-item label="乙方" prop="seller" >
+        <el-input placeholder="请输入乙方" v-model="state.queryForm.seller" />
+      </el-form-item>
+      <el-form-item label="签订日期" prop="signingTime">
+      <el-date-picker type="date" :value-format="dateStr" placeholder="请输入签订日期" v-model="state.queryForm.signingTime"></el-date-picker>
+      </el-form-item>
+      <el-form-item label="项目名称" prop="projectName" >
+        <el-input placeholder="请输入项目名称" v-model="state.queryForm.projectName" />
+      </el-form-item>
+          <el-form-item>
+            <el-button icon="search" type="primary" @click="getDataList">
+              查询
+            </el-button>
+            <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-row>
       <el-row>
         <div class="mb8" style="width: 100%">
           <el-button icon="folder-add" type="primary" class="ml10" @click="formDialogRef.openDialog()"
--- src/views/iams/iamsShelf/index.vue
+++ src/views/iams/iamsShelf/index.vue
@@ -1,60 +1,84 @@
 <template>
   <div class="layout-padding">
     <div class="layout-padding-auto layout-padding-view">
+      <el-row v-show="showSearch">
+        <el-form ref="queryRef" :inline="true" :model="state.queryForm" @keyup.enter="getDataList">
+          <el-form-item label="所在机房" prop="room">
+            <el-input v-model="state.queryForm.room" placeholder="请选择所在机房"/>
+          </el-form-item>
+          <el-form-item label="所在微模块" prop="module">
+            <el-input v-model="state.queryForm.module" placeholder="请选择所在微模块"/>
+          </el-form-item>
+          <el-form-item label="所在机柜" prop="cabinet">
+            <el-input v-model="state.queryForm.cabinet" placeholder="请选择所在机柜"/>
+          </el-form-item>
+          <el-form-item label="项目名称" prop="projectName">
+            <el-input v-model="state.queryForm.projectName" placeholder="请输入项目名称"/>
+          </el-form-item>
+          <el-form-item>
+            <el-button icon="search" type="primary" @click="getDataList">
+              查询
+            </el-button>
+            <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-row>
       <el-row>
         <div class="mb8" style="width: 100%">
-          <el-button icon="folder-add" type="primary" class="ml10" @click="formDialogRef.openDialog()"
-            v-auth="'iams_iamsShelf_add'">
+          <el-button v-auth="'iams_iamsShelf_add'" class="ml10" icon="folder-add" type="primary"
+                     @click="formDialogRef.openDialog()">
             新 增
           </el-button>
-          <el-button plain :disabled="multiple" icon="Delete" type="primary"
-            v-auth="'iams_iamsShelf_del'" @click="handleDelete(selectObjs)">
+          <el-button v-auth="'iams_iamsShelf_del'" :disabled="multiple" icon="Delete" plain
+                     type="primary" @click="handleDelete(selectObjs)">
             删除
           </el-button>
           <right-toolbar v-model:showSearch="showSearch" :export="'iams_iamsShelf_export'"
-                @exportExcel="exportExcel" class="ml10 mr20" style="float: right;"
-            @queryTable="getDataList"></right-toolbar>
+                         class="ml10 mr20" style="float: right;" @exportExcel="exportExcel"
+                         @queryTable="getDataList"></right-toolbar>
         </div>
       </el-row>
-      <el-table :data="state.dataList" v-loading="state.loading" border 
-        :cell-style="tableStyle.cellStyle" :header-cell-style="tableStyle.headerCellStyle"
-				@selection-change="selectionChangHandle"
-        @sort-change="sortChangeHandle">
-        <el-table-column type="selection" width="40" align="center" />
-        <el-table-column type="index" label="#" width="40" />
-        <el-table-column prop="room" label="所在机房" show-overflow-tooltip/>
-        <el-table-column prop="module" label="所在微模块" show-overflow-tooltip/>
-        <el-table-column prop="cabinet" label="所在机柜" show-overflow-tooltip/>
-        <el-table-column prop="shelf" label="所在机架" show-overflow-tooltip/>
-<!--          <el-table-column prop="cabinetId" label="机柜编号"  show-overflow-tooltip/>-->
-        <el-table-column prop="role" label="设备角色" show-overflow-tooltip/>
-<!--          <el-table-column prop="assetId" label="资产编号"  show-overflow-tooltip/>-->
-<!--        <el-table-column prop="shelfName" label="所在机架" show-overflow-tooltip/>-->
+      <el-table v-loading="state.loading" :cell-style="tableStyle.cellStyle" :data="state.dataList"
+                :header-cell-style="tableStyle.headerCellStyle" border
+                @selection-change="selectionChangHandle"
+                @sort-change="sortChangeHandle">
+        <el-table-column align="center" type="selection" width="40"/>
+        <el-table-column label="#" type="index" width="40"/>
+        <el-table-column label="所在机房" prop="room" show-overflow-tooltip/>
+        <el-table-column label="所在微模块" prop="module" show-overflow-tooltip/>
+        <el-table-column label="所在机柜" prop="cabinet" show-overflow-tooltip/>
+        <el-table-column label="所在机架" prop="shelf" show-overflow-tooltip/>
+        <!--          <el-table-column prop="cabinetId" label="机柜编号"  show-overflow-tooltip/>-->
+        <el-table-column label="设备角色" prop="role" show-overflow-tooltip/>
+        <!--          <el-table-column prop="assetId" label="资产编号"  show-overflow-tooltip/>-->
+        <!--        <el-table-column prop="shelfName" label="所在机架" show-overflow-tooltip/>-->
 
-<!--          <el-table-column prop="unitStart" label="占用机架起始位置"  show-overflow-tooltip/>-->
-<!--          <el-table-column prop="unitEnd" label="占用机架结束位置"  show-overflow-tooltip/>-->
+        <!--          <el-table-column prop="unitStart" label="占用机架起始位置"  show-overflow-tooltip/>-->
+        <!--          <el-table-column prop="unitEnd" label="占用机架结束位置"  show-overflow-tooltip/>-->
         <el-table-column label="操作" width="150">
           <template #default="scope">
-            <el-button icon="edit-pen" text type="primary" v-auth="'iams_iamsShelf_edit'"
-              @click="formDialogRef.openDialog(scope.row.id)">编辑</el-button>
-            <el-button icon="delete" text type="primary" v-auth="'iams_iamsShelf_del'" @click="handleDelete([scope.row.id])">删除</el-button>
+            <el-button v-auth="'iams_iamsShelf_edit'" icon="edit-pen" text type="primary"
+                       @click="formDialogRef.openDialog(scope.row.id)">编辑
+            </el-button>
+            <el-button v-auth="'iams_iamsShelf_del'" icon="delete" text type="primary"
+                       @click="handleDelete([scope.row.id])">删除
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
-      <pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" v-bind="state.pagination" />
+      <pagination v-bind="state.pagination" @size-change="sizeChangeHandle" @current-change="currentChangeHandle"/>
     </div>
 
     <!-- 编辑、新增  -->
-    <form-dialog ref="formDialogRef" @refresh="getDataList(false)" />
+    <form-dialog ref="formDialogRef" @refresh="getDataList(false)"/>
 
   </div>
 </template>
 
-<script setup lang="ts" name="systemIamsShelf">
-import { BasicTableProps, useTable } from "/@/hooks/table";
-import { fetchList, delObjs } from "/@/api/iams/iamsShelf";
-import { useMessage, useMessageBox } from "/@/hooks/message";
-import { useDict } from '/@/hooks/dict';
+<script lang="ts" name="systemIamsShelf" setup>
+import {BasicTableProps, useTable} from "/@/hooks/table";
+import {delObjs, fetchList} from "/@/api/iams/iamsShelf";
+import {useMessage, useMessageBox} from "/@/hooks/message";
 
 // 引入组件
 const FormDialog = defineAsyncComponent(() => import('./form.vue'));
@@ -81,7 +105,7 @@ const {
   sizeChangeHandle,
   sortChangeHandle,
   downBlobFile,
-	tableStyle
+  tableStyle
 } = useTable(state)
 
 // 清空搜索条件
@@ -95,12 +119,12 @@ const resetQuery = () => {
 
 // 导出excel
 const exportExcel = () => {
-  downBlobFile('/iams/iamsShelf/export',Object.assign(state.queryForm, { ids: selectObjs }), 'iamsShelf.xlsx')
+  downBlobFile('/iams/iamsShelf/export', Object.assign(state.queryForm, {ids: selectObjs}), 'iamsShelf.xlsx')
 }
 
 // 多选事件
 const selectionChangHandle = (objs: { id: string }[]) => {
-  selectObjs.value = objs.map(({ id }) => id);
+  selectObjs.value = objs.map(({id}) => id);
   multiple.value = !objs.length;
 };
 
