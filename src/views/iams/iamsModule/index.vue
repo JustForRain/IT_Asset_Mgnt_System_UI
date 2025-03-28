475ebcd6694d936a5d8edd38168519dd520dc486
yu.xia <rain@pcrain.com>
2025-03-28 16:47:50 +0800

    完成微模块管理模块条件查询接口前后端联调

--- src/views/iams/iamsModule/index.vue
+++ src/views/iams/iamsModule/index.vue
@@ -1,55 +1,85 @@
 <template>
   <div class="layout-padding">
     <div class="layout-padding-auto layout-padding-view">
+      <el-row v-show="showSearch">
+        <el-form ref="queryRef" :inline="true" :model="state.queryForm" @keyup.enter="getDataList">
+          <el-form-item label="所属机房" prop="roomName">
+            <el-select v-model="state.queryForm.roomName" :remote-method="fetchRoomListHandler" allow-create clearable
+                       filterable
+                       placeholder="请选择所属机房" remote reserve-keyword @change="roomChangeHandler">
+              <el-option v-for="item in roomList" :label="item.label" :value="item.label"/>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="微模块名称" prop="id">
+            <el-select v-model="state.queryForm.id" :disabled="moduleNameLoading"
+                       :remote-method="fetchModuleListHandler"
+                       allow-create clearable
+                       filterable placeholder="请选择微模块名称" remote reserve-keyword>
+              <el-option v-for="item in moduleList" :label="item.label" :value="item.value"/>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button icon="search" type="primary" @click="getDataList">
+              查询
+            </el-button>
+            <el-button icon="Refresh" @click="resetQuery">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </el-row>
       <el-row>
         <div class="mb8" style="width: 100%">
-          <el-button icon="folder-add" type="primary" class="ml10" @click="formDialogRef.openDialog()"
-            v-auth="'iams_iamsModule_add'">
+          <el-button v-auth="'iams_iamsModule_add'" class="ml10" icon="folder-add" type="primary"
+                     @click="formDialogRef.openDialog()">
             新 增
           </el-button>
-          <el-button plain :disabled="multiple" icon="Delete" type="primary"
-            v-auth="'iams_iamsModule_del'" @click="handleDelete(selectObjs)">
+          <el-button v-auth="'iams_iamsModule_del'" :disabled="multiple" icon="Delete" plain
+                     type="primary" @click="handleDelete(selectObjs)">
             删除
           </el-button>
           <right-toolbar v-model:showSearch="showSearch" :export="'iams_iamsModule_export'"
-                @exportExcel="exportExcel" class="ml10 mr20" style="float: right;"
-            @queryTable="getDataList"></right-toolbar>
+                         class="ml10 mr20" style="float: right;" @exportExcel="exportExcel"
+                         @queryTable="getDataList"></right-toolbar>
         </div>
       </el-row>
-      <el-table :data="state.dataList" v-loading="state.loading" border 
-        :cell-style="tableStyle.cellStyle" :header-cell-style="tableStyle.headerCellStyle"
-				@selection-change="selectionChangHandle"
-        @sort-change="sortChangeHandle">
-        <el-table-column type="selection" width="40" align="center" />
-        <el-table-column type="index" label="#" width="40" />
-          <el-table-column prop="roomName" label="所属机房"  show-overflow-tooltip/>
-          <el-table-column prop="name" label="微模块名称"  show-overflow-tooltip/>
-          <el-table-column prop="size" label="机柜数量"  show-overflow-tooltip/>
+      <el-table v-loading="state.loading" :cell-style="tableStyle.cellStyle" :data="state.dataList"
+                :header-cell-style="tableStyle.headerCellStyle" border
+                @selection-change="selectionChangHandle"
+                @sort-change="sortChangeHandle">
+        <el-table-column align="center" type="selection" width="40"/>
+        <el-table-column label="#" type="index" width="40"/>
+        <el-table-column label="所属机房" prop="roomName" show-overflow-tooltip/>
+        <el-table-column label="微模块名称" prop="name" show-overflow-tooltip/>
+        <el-table-column label="机柜数量" prop="size" show-overflow-tooltip/>
         <el-table-column label="操作" width="150">
           <template #default="scope">
-            <el-button icon="edit-pen" text type="primary" v-auth="'iams_iamsModule_edit'"
-              @click="formDialogRef.openDialog(scope.row.id)">编辑</el-button>
-            <el-button icon="delete" text type="primary" v-auth="'iams_iamsModule_del'" @click="handleDelete([scope.row.id])">删除</el-button>
-            <el-button icon="delete" text type="primary" v-auth="'iams_iamsModule_view'" @click="moduleDetailDialogRef.openDialog(scope.row.id)">查看</el-button>
+            <el-button v-auth="'iams_iamsModule_edit'" icon="edit-pen" text type="primary"
+                       @click="formDialogRef.openDialog(scope.row.id)">编辑
+            </el-button>
+            <el-button v-auth="'iams_iamsModule_del'" icon="delete" text type="primary"
+                       @click="handleDelete([scope.row.id])">删除
+            </el-button>
+            <el-button v-auth="'iams_iamsModule_view'" icon="delete" text type="primary"
+                       @click="moduleDetailDialogRef.openDialog(scope.row.id)">查看
+            </el-button>
           </template>
         </el-table-column>
       </el-table>
-      <pagination @size-change="sizeChangeHandle" @current-change="currentChangeHandle" v-bind="state.pagination" />
+      <pagination v-bind="state.pagination" @size-change="sizeChangeHandle" @current-change="currentChangeHandle"/>
     </div>
 
     <!-- 编辑、新增  -->
-    <form-dialog ref="formDialogRef" @refresh="getDataList(false)" />
+    <form-dialog ref="formDialogRef" @refresh="getDataList(false)"/>
 
-    <module-detail-dialog ref="moduleDetailDialogRef" />
+    <module-detail-dialog ref="moduleDetailDialogRef"/>
 
   </div>
 </template>
 
-<script setup lang="ts" name="systemIamsModule">
-import { BasicTableProps, useTable } from "/@/hooks/table";
-import { fetchList, delObjs } from "/@/api/iams/iamsModule";
-import { useMessage, useMessageBox } from "/@/hooks/message";
-import { useDict } from '/@/hooks/dict';
+<script lang="ts" name="systemIamsModule" setup>
+import {BasicTableProps, useTable} from "/@/hooks/table";
+import {delObjs, fetchList, fetchList as fetchModuleList} from "/@/api/iams/iamsModule";
+import {fetchList as fetchRoomList} from '/@/api/iams/iamsRoom'
+import {useMessage, useMessageBox} from "/@/hooks/message";
 
 // 引入组件
 const FormDialog = defineAsyncComponent(() => import('./form.vue'));
@@ -58,7 +88,7 @@ const ModuleDetailDialog = defineAsyncComponent(() => import('./moduleDetail.vue
 
 // 定义变量内容
 const formDialogRef = ref()
-const moduleDetailDialogRef =ref()
+const moduleDetailDialogRef = ref()
 // 搜索变量
 const queryRef = ref()
 const showSearch = ref(true)
@@ -78,7 +108,7 @@ const {
   sizeChangeHandle,
   sortChangeHandle,
   downBlobFile,
-	tableStyle
+  tableStyle
 } = useTable(state)
 
 // 清空搜索条件
@@ -92,12 +122,12 @@ const resetQuery = () => {
 
 // 导出excel
 const exportExcel = () => {
-  downBlobFile('/iams/iamsModule/export',Object.assign(state.queryForm, { ids: selectObjs }), 'iamsModule.xlsx')
+  downBlobFile('/iams/iamsModule/export', Object.assign(state.queryForm, {ids: selectObjs}), 'iamsModule.xlsx')
 }
 
 // 多选事件
 const selectionChangHandle = (objs: { id: string }[]) => {
-  selectObjs.value = objs.map(({ id }) => id);
+  selectObjs.value = objs.map(({id}) => id);
   multiple.value = !objs.length;
 };
 
@@ -117,4 +147,70 @@ const handleDelete = async (ids: string[]) => {
     useMessage().error(err.msg);
   }
 };
+
+const loading = ref(false)
+const moduleNameLoading = ref(false)
+//所属机房远程搜索
+const roomList = ref([])
+const moduleList = ref([])
+
+const fetchRoomListHandler = (val: string) => {
+  try {
+    loading.value = true;
+    fetchRoomList({"name": val}).then((res) => {
+      roomList.value = []
+      res.data.records.forEach((item: any) => {
+        roomList.value.push({
+          label: item.name,
+          value: item.id
+        })
+      })
+    })
+  } finally {
+    loading.value = false;
+  }
+}
+
+//微模块远程搜索
+const fetchModuleListHandler = (val: string) => {
+  try {
+    loading.value = true;
+    fetchModuleList({"name": val}).then((res) => {
+      moduleList.value = []
+      res.data.records.forEach((item: any) => {
+        moduleList.value.push({
+          label: item.name,
+          value: item.id
+        })
+      })
+    })
+  } finally {
+    loading.value = false;
+  }
+}
+
+const roomChangeHandler = (roomName: string) => {
+  //根据val查询微模块列表
+  fetchModuleList({"roomName": roomName}).then((res) => {
+    if (res.data.total > 0) {
+      moduleList.value = []
+      res.data.records.forEach((item: any) => {
+        moduleList.value.push({
+          label: item.name,
+          value: item.id
+        })
+      })
+    } else {
+      moduleList.value = []
+    }
+  })
+}
+
+onMounted(() => {
+  roomList.value = [];
+  moduleList.value = [];
+  fetchModuleListHandler()
+  fetchRoomListHandler()
+})
+
 </script>
